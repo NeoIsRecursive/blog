124197cfc17fbaee45cae13381325ef4b84af525
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2022-05-04 22:17:51 +0200

    Add api key and dateset to env file

--- src/utils/GetSanityUrl.js
+++ src/utils/GetSanityUrl.js
@@ -1,6 +1,6 @@
 const GetSanityUrl = (query) => {
-  const PROJECT_ID = "b139pzd6";
-  const DATASET = "production";
+  const { REACT_APP_PROJECT_ID: PROJECT_ID, REACT_APP_DATASET: DATASET } =
+    process.env;
   const QUERY = encodeURIComponent(query);
   return `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
 };
--- src/utils/client.js
+++ src/utils/client.js
@@ -1,8 +1,11 @@
 import sanityClient from "@sanity/client";
 
+const { REACT_APP_PROJECT_ID: PROJECT_ID, REACT_APP_DATASET: DATASET } =
+  process.env;
+
 export default sanityClient({
-  projectId: "b139pzd6",
-  dataset: "production",
+  projectId: PROJECT_ID,
+  dataset: DATASET,
   apiVersion: "2021-03-25",
   useCdn: true,
 });
